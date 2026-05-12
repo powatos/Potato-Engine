96e229cf35025b8ef52b555fcab9a1db2843246c
Sreejith <167687638+powatos@users.noreply.github.com>
2026-05-12 00:51:36 +0000

    refactor: extracted level read/write methods via template

--- Source/Internal/Public/Core/Datastore/PersistentLevel.hpp
+++ Source/Internal/Public/Core/Datastore/PersistentLevel.hpp
@@ -13,6 +13,12 @@ struct PersistentLevel
     // @returns if all static actors were loaded successfully
     bool LoadStaticActors();
 
+    template <typename Type>
+    Type GetData(std::string key) const;
+
+    template <typename Type>
+    void WriteData(std::string key, Type value);
+
     int GetIntData( std::string key );
     float GetFloatData( std::string key );
     std::string GetStringData( std::string key );
@@ -28,3 +34,35 @@ private:
     std::filesystem::path saveFileAbsPath;
 };
 
+
+template <typename Type>
+Type PersistentLevel::GetData(std::string key) const {
+    if      constexpr (std::is_same_v<Type, int>) {
+        return GetIntData(key);
+    }
+    else if constexpr (std::is_same_v<Type, float>) {
+        return GetFloatData(key);
+    }
+    else if constexpr (std::is_same_v<Type, std::string>) {
+        return GetStringData(key);
+    }
+    else if constexpr (std::is_same_v<Type, Vector2>) {
+        return GetVector2Data(key);
+    }
+}
+
+template <typename Type>
+void PersistentLevel::WriteData(std::string key, Type value) {
+    if      constexpr (std::is_same_v<Type, int>) {
+        WriteIntData(key, value);
+    }
+    else if constexpr (std::is_same_v<Type, float>) {
+        WriteFloatData(key, value);
+    }
+    else if constexpr (std::is_same_v<Type, std::string>) {
+        WriteStringData(key, value);
+    }
+    else if constexpr (std::is_same_v<Type, Vector2>) {
+        WriteVector2Data(key, value);
+    }
+}
