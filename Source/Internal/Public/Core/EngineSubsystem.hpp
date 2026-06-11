8fe91beba7e4e630f3bcacc54b0fdced48cf8a61
Sreejith <167687638+powatos@users.noreply.github.com>
2026-06-11 22:59:37 +0000

    docs: documentation update

--- Source/Internal/Public/Core/EngineSubsystem.hpp
+++ Source/Internal/Public/Core/EngineSubsystem.hpp
@@ -24,6 +24,13 @@ template<typename T>
 class EngineSubsystem : public IEngineSubsystem, public Singleton<T> 
 {};
 
+/**
+ * @def ENGINE_SUBSYSTEM(type)
+ * @brief Adds singleton as a friend of the class to ensure proper Get retrieval
+ * @details Call this on the first line of the class header
+ * @note This macro is intended for internal use
+ * @param type class name
+ */
 #define ENGINE_SUBSYSTEM(type) \
 friend class Singleton<type>;
 
